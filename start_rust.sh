45eac019de09c7e65b783b4829cbb2a569416013
tobiaspedersen <tobiped@icloud.com>
2019-11-28 22:31:42 +0100

    Update start_rust.sh

--- start_rust.sh
+++ start_rust.sh
@@ -133,8 +133,10 @@ elif [ "$MAPSIZE" = "large" ]; then
 MPSIZE="6000"
 elif [ "$MAPSIZE" = "massive" ]; then
 MPSIZE="8000"
+elif [ "$MAPSIZE" -ge "1000" && "$MAPSIZE" -le "8000"]; then
+MPSIZE="$MAPSIZE"
 else
-echo "Error: Please select map size"
+echo "Error: Please select a valid map size. Valid map sizes are: tiny, small, medium, large, massive or a number between 1000-8000"
 exit
 fi
 
